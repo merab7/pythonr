4f7a7264106361263d8723c2acbd84f0d925f20d
merab7 <merabtodua7@gmail.com>
2024-12-12 22:32:25 +0400

    asyncoio end

--- asyncio_learn.py
+++ asyncio_learn.py
@@ -9,16 +9,21 @@ async def fetch_data(id, sleep_time):
 
 
 async def main():
+    # first way declearin eatch task separatly
     # Create task for running coroutines concurrently
-    task1 = asyncio.create_task(fetch_data(1, 2))
-    task2 = asyncio.create_task(fetch_data(2, 3))
-    task3 = asyncio.create_task(fetch_data(3, 1))
-
-    result1 = await task1
-    result2 = await task2
-    result3 = await task3
-
-    print(result1, result2, result3)
+    # task1 = asyncio.create_task(fetch_data(1, 2))
+    # task2 = asyncio.create_task(fetch_data(2, 3))
+    # task3 = asyncio.create_task(fetch_data(3, 1))
+    #
+    # result1 = await task1
+    # result2 = await task2
+    # result3 = await task3
+    #
+    # print(result1, result2, result3)
+    # second way using gether function and creating list of tasks in one line
+    results = await asyncio.gather(fetch_data(1, 2), fetch_data(2, 1), fetch_data(3, 3))
+    for result in results:
+        print(f"Received results: {result}")
 
 
 # after main() dunction is colled it returns coroutine object with
